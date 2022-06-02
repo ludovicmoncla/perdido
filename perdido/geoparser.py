e6466b53111332160fceacfee1e655da316b4eed
lmoncla <moncla.ludovic@gmail.com>
2022-06-02 22:42:26 +0200

    Update geoparser.py

--- perdido/geoparser.py
+++ perdido/geoparser.py
@@ -5,7 +5,6 @@ import lxml.etree as etree
 
 
 #TODO deplacer cette fonction dans utils/xx.py
-#TODO retourner le tei et le geojson avec le service geoparsing
 def getResult(jsonStr, field='result', outputFormat='json'):
     if json.loads(jsonStr.text)['status'] == "success":
         if outputFormat == 'html':
@@ -18,25 +17,90 @@ def getResult(jsonStr, field='result', outputFormat='json'):
         return None
 
 
+#TODO deplacer cette fonction dans utils/xx.py 
+def get_w_content(element):
+    content = ""
+    for w in element.findall('.//w'):
+        content += w.text + " "
+    return content.strip()
+
+
+
+def parent_exists(elt, parent_name):
+    try:
+        parent_node = next(elt.iterancestors())
+        if parent_name is not None:
+            if parent_node.tag == parent_name:
+                if 'start' in parent_node.attrib:
+                    return True
+        return parent_exists(parent_node, parent_name)
+    except StopIteration:
+        return False
+
+
+
+#TODO deplacer cette fonction dans utils/xx.py 
+def get_tokens(elt):
+    tokens = []
+    for elt in elt.findall('.//w'):
+        lemma = elt.get('lemma') if 'lemma' in elt.attrib else  ""
+        pos = elt.get('type') if 'type' in elt.attrib else  ""
+        tokens.append(Token(elt.text, lemma, pos))
+    return tokens
+
+
+def get_entity(elt):
+    text = get_w_content(elt)
+    tag = elt.get('type') if 'type' in elt.attrib else  ""
+    tokens = get_tokens(elt)
+    parent = elt.getparent()
+    #TODO get and return lat/lng if it is a place
+    return text, tokens, tag, parent
+
+
+def get_entities(elt):
+    entities = []
+    for elt in elt.findall('.//name'):
+        text, tokens, tag, parent = get_entity(elt)
+        entities.append(Entity(text, tokens, tag, parent))
+    return entities
+
+
+def get_nested_entities(elt):
+    nestedEntities = []
+    for elt in elt.findall(".//rs[@type='ene']/rs[@subtype='ene']"):
+        text, tokens, tag, parent = get_entity(elt)
+        child = elt.xpath(".//*[self::rs or self::name]")[0]
+        ne = get_entities(elt)
+        #TODO get the nesting level
+        nestedEntities.append(Entity(text, tokens, tag, parent, child, ne, 1))
+    return nestedEntities
+
+
 class Entity:
-    def __init__(self, text, tokens, tag, parent=None, child=None, level=0):
+    def __init__(self, text, tokens, tag, parent=None, child=None, ne=None, level=0, lat=None, lng=None):
 
         self.text = text
         self.tokens = tokens
         self.tag = tag
-    
+
+        self.lat = lat
+        self.lng = lng
+
         self.parent = parent
         self.child = child
 
         self.level = level # find a better name?
+        self.ne = ne
+        #self.sent = sent # sentence in which the entity occurs, useful?
         #...
 
 
+# TODO
 class Toponym: 
-    def __init__(self, name, ):
+    def __init__(self, name):
 
         self.name = name
-        
 
 
 class Token:
@@ -45,9 +109,9 @@ class Token:
         self.text = text
         self.lemma = lemma
         self.pos = pos
+
+        # position, start, end ?
         
-
-
 
 #TODO deplacer cette classe dans un nouveau fichier perdido.py
 class Perdido:
@@ -59,29 +123,31 @@ class Perdido:
         self.geojson = None
         
         self.ne = []
-        self.ene = []
+        self.nne = [] # nested named entities
         self.tokens = []
 
         self.toponyms = []
 
-        # TODO ajouter le parsing xml pour récupérer les différents éléments :
-
+    
 
     def parseTEI(self):
         root = etree.fromstring(self.tei)
-        for w in root.findall('.//w'):
-            lemma = w.get('lemma') if 'lemma' in w.attrib else  ""
-            pos = w.get('type') if 'type' in w.attrib else  ""
-            self.tokens.append(Token(w.text, lemma, pos))
+        
+        self.tokens = get_tokens(root)
+        self.ne = get_entities(root)
+        
+        for elt in root.findall(".//rs[@type='ene']/rs[@subtype='ene']"):
+            text = get_w_content(elt)
+            tag = elt.get('type') if 'type' in elt.attrib else  ""
+            tokens = get_tokens(elt)
+            parent = elt.getparent()
+            child = elt.xpath(".//*[self::rs or self::name]")[0]
+            ne = get_entities(elt)
+            #TODO get the nesting level
+            self.nne.append(Entity(text, tokens, tag, parent, child, ne, 1))
 
         
-        for rs in root.findall('.//name'):
 
-            #self.ne.append(Token(rs.text, lemma, pos))
-            pass        
-
-    #TODO ajouter une méthode qui retourne le contenu txt d'une balise tei (boucle sur les balises w)
-    
     #TODO ajouter les méthodes de display
 
 
@@ -116,7 +182,7 @@ class Geoparser:
         r = requests.post(self._urlAPI + self._serviceGeoparsing, params=parameters)
         
         res = Perdido()
-        res.txt = content
+        res.text = content
         res.tei = getResult(r, 'xml-tei', 'xml')
         res.geojson = getResult(r, 'geojson')
 

0cc770a13378ea49b12d138e929f927caad83e1f
Ludovic Moncla <300641+ludovicmoncla@users.noreply.github.com>
2022-06-03 22:47:46 +0200

    Delete build/lib/perdido directory

--- build/lib/perdido/__init__.py
+++
@@ -1,3 +0,0 @@
-from perdido.utils.webservices import WebService
-from perdido.utils.xml import *
-from perdido.utils.map import *
--- build/lib/perdido/geocoder.py
+++
@@ -1,6 +0,0 @@
-
-
-class Geocoder:
-    def __init__(self):
-        pass
-
